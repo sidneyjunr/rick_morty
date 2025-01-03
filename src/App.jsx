04478f02b4236c59f712d932891380e8ec751c7b
Sidney Junior <sidneyferreirabaptistajunior3@gmail.com>
2025-01-03 03:29:14 -0300

    Ajeitando algo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { Page404 } from './pages/Page404'
 import { GlobalStyles } from "./GlobalStyles"
@@ -7,14 +7,14 @@ import { Detalhes } from "./pages/Detalhes"
 function App() {
    return (
     <>
-      <BrowserRouter>
+      <HashRouter>
       <GlobalStyles/>
         <Routes>
           <Route path="detalhes/:id" element={<Detalhes/>}></Route>
           <Route path="/" element={<Home/>}></Route>
           <Route path="*" element={<Page404/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
