4445811bd9478d3e399c721aeef6a9945c979301
Sidney <sidneyferreirabaptistajunior3@gmail.com>
2025-03-21 13:46:55 -0300

    commit de atualização do deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rick_morty/',  // Adicione o caminho base do seu repositório no GitHub Pages
   plugins: [react()],
 })
